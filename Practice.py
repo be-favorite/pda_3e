1c754994aeacfe6228c1cabc2d362c957ec9e760
be-favorite <favorite@kakao.com>
2023-04-15 15:03:59 +0900

    update study log

--- Practice.py
+++ Practice.py
@@ -10,3 +10,12 @@ type(dt)
 type(dt.strftime("%Y-%m-%d %H:%M"))
 
 type(datetime.strptime("20091031", "%Y%m%d"))
+
+sequence = [1, 2, None, 4, None, 5]
+total = 0
+for value in sequence:
+    if value is None:
+        continue
+    total += value
+    
+list(range(0, 10))
